1e1cecb5ceb6c66aad728249bce4eb649c5e0e0e
jonitoh <jordannt94@gmail.com>
2022-02-04 00:31:47 +0100

    update package.json for client and server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+dotenv.config({ path: '../.env' })
+
+const port = process.env.PORT || 4000;
+
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// serve the react app files
+app.use(express.static(`${__dirname}/client/build`));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
